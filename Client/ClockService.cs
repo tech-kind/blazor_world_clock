160a8d708807e21f0da27be1619456d840242dad
TechKind <ken_hayakawa@tech-kind.biz>
2022-01-06 15:44:49 +0900

    update step.12

---
+++ Client/ClockService.cs
@@ -0,0 +1,25 @@
+﻿using BlazorWorldClock.Shared;
+
+namespace BlazorWorldClock.Client
+{
+    public class ClockService
+    {
+        private List<Clock> Clocks { get; } = new()
+        {
+            new() { Name = "札幌", TimeZoneId = "Asia/Tokyo"},
+            new() { Name = "バンコク", TimeZoneId = "Asia/Bangkok"},
+            new() { Name = "シアトル", TimeZoneId = "America/Los_Angeles"},
+        };
+
+        public async ValueTask<IEnumerable<Clock>> GetClocksAsync()
+        {
+            return await ValueTask.FromResult(this.Clocks);
+        }
+
+        public async ValueTask AddClockAsync(Clock clock)
+        {
+            this.Clocks.Add(clock);
+            await ValueTask.CompletedTask;
+        }
+    }
+}
